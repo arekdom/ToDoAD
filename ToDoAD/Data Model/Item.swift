9eb9c22a5a5062bd77373fd47c64a8e7a65aab46
Arek <arek@magnat.local>
2018-03-20 18:44:13 +0000

    issue fixed

---
+++ ToDoAD/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoAD
+//
+//  Created by Arek on 19/03/2018.
+//  Copyright © 2018 Arek. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
